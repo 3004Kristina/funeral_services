04a0d0ad550efd08e5c0181ed25b4c1522e550b2
sakhalin <73.vorobyev@gmail.com>
2020-12-08 14:57:16 +0300

    init

---
+++ .footer.php
@@ -0,0 +1,10 @@
+<!--FOOTER START-->
+<footer class="footer_wrapper">
+</footer>
+<!--FOOTER END-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/remodal/1.1.1/remodal.min.js" integrity="sha512-a/KwXZUMuN0N2aqT/nuvYp6mg1zKg8OfvovbIlh4ByLw+BJ4sDrJwQM/iSOd567gx+yS0pQixA4EnxBlHgrL6A==" crossorigin="anonymous"></script>
+<script src="/js/script.js"></script>
+</body>
+</html>
