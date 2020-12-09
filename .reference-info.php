887e06486f72b5d7ee5e7f191bcc83e724f513aa
sakhalin <73.vorobyev@gmail.com>
2020-12-09 17:06:12 +0300

    add page morgues & section reference info

---
+++ .reference-info.php
@@ -0,0 +1,43 @@
+<!--SECTION REFERENCE INFO START-->
+<div class="reference_info_wrapper">
+    <div class="container">
+        <div class="reference_info">
+            <div class="reference_info_block">
+                <h2>Справочная информация</h2>
+                <div class="reference_info_list">
+                    <div class="reference_info_item">
+                        <img src="/img/icons/morgue.svg" alt="icon">
+                        <a href="#">МОРГИ</a>
+                    </div>
+
+                    <div class="reference_info_item">
+                        <img src="/img/icons/cemetery.svg" alt="icon">
+                        <a href="#">Кладбища</a>
+                    </div>
+
+                    <div class="reference_info_item">
+                        <img src="/img/icons/columbariy.svg" alt="icon">
+                        <a href="#">Колумбарии</a>
+                    </div>
+
+                    <div class="reference_info_item">
+                        <img src="/img/icons/fire.svg" alt="icon">
+                        <a href="#">Крематории</a>
+                    </div>
+                </div>
+            </div>
+            <div class="reference_info_block">
+                <div class="reference_info_content">
+                    <h4>Заголовок</h4>
+                    <p>
+                        This is Photoshop's version  of Lorem  Ipsum. Proin
+                        gravida nibh vel velit auctor aliquet. Aenean sollicitudin,
+                        lorem quis bibendum auctor, nisi elit consequat ipsum
+                    </p>
+                    <button class="btn">Московская область</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--SECTION REFERENCE INFO END-->
