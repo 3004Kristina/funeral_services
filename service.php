afb5cc7e0f70db90f545a7b059e7507db124414a
3004Kristina <kristina.usova@gmail.com>
2020-12-17 21:32:49 +0300

    add table

--- service.php
+++ service.php
@@ -39,6 +39,235 @@
 </section>
 <!--SECTION FUNERAL ORGANIZATION PAGE INFO CONTENT END-->
 
+<!--SECTION SERVICE PAGE FUNERAL ARRANGEMENTS START-->
+<section class="service_page_arrangements_wrapper">
+    <div class="container">
+        <h2>Порядок организации похорон</h2>
+        <div class="service_page_arrangements">
+            <div class="advantages_item">
+                <img src="/img/icons/s1.png" alt="icon">
+                <h2>Консультация</h2>
+            </div>
+            <div class="advantages_item">
+                <img src="/img/icons/s2.png" alt="icon">
+                <h2>Оформление<br> документов</h2>
+
+            </div>
+            <div class="advantages_item">
+                <img src="/img/icons/s3.png" alt="icon">
+                <h2>Подготовка<br>
+                    к церемонии</h2>
+
+            </div>
+            <div class="advantages_item">
+                <img src="/img/icons/s4.png" alt="icon">
+                <h2>Панихида</h2>
+            </div>
+            <div class="advantages_item">
+                <img src="/img/icons/s5.png" alt="icon">
+                <h2>Захоронение<br>
+                    кремация</h2>
+            </div>
+            <div class="advantages_item">
+                <img src="/img/icons/s6.png" alt="icon">
+                <h2>Поминовение</h2>
+            </div>
+        </div>
+    </div>
+</section>
+<!--SECTION SERVICE PAGE FUNERAL ARRANGEMENTS END-->
+
+<!--SECTION MAIN SERVICES PAGE PRICE START-->
+<section class="service_page_price_wrapper">
+    <div class="container">
+        <div class="prices_contract_wrapper">
+            <div class="prices_content">
+                <ul class="prices_navigation_tabs">
+                    <li><a href="#tab1" class="active"><span>Стоимость захоронения</span></a></li>
+                    <li><a href="#tab2"><span>Стоимость кремации</span></a></li>
+                </ul>
+
+                <div class="prices_content_tabs">
+                    <div class="tab" id="tab1">
+                        <a href="#" class="tab_item">
+                            <div class="descr">
+                                <h4>Базовый</h4>
+                                <p>
+                                    This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh
+                                    vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
+                                    nisi elit consequat
+                                </p>
+                            </div>
+                            <div class="price">
+                                13 990
+                                <i class="fa fa-rub" aria-hidden="true"></i>
+                            </div>
+                        </a>
+                        <a href="#" class="tab_item">
+                            <div class="descr">
+                                <h4>Стандарт</h4>
+                                <p>
+                                    This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh
+                                    vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
+                                    nisi elit consequat
+                                </p>
+                            </div>
+                            <div class="price">
+                                13 990
+                                <i class="fa fa-rub" aria-hidden="true"></i>
+                            </div>
+                        </a>
+                        <a href="#" class="tab_item">
+                            <div class="descr">
+                                <h4>Премиум</h4>
+                                <p>
+                                    This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh
+                                    vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
+                                    nisi elit consequat
+                                </p>
+                            </div>
+                            <div class="price">
+                                13 990
+                                <i class="fa fa-rub" aria-hidden="true"></i>
+                            </div>
+                        </a>
+                        <a href="#" class="tab_item">
+                            <div class="descr">
+                                <h4>VIP</h4>
+                                <p>
+                                    This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh
+                                    vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
+                                    nisi elit consequat
+                                </p>
+                            </div>
+                            <div class="price">
+                                13 990
+                                <i class="fa fa-rub" aria-hidden="true"></i>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="tab" id="tab2" style="display: none">
+                        <a href="#" class="tab_item">
+                            <div class="descr">
+                                <h4>Базовый</h4>
+                                <p>
+                                    This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh
+                                    vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
+                                    nisi elit consequat
+                                </p>
+                            </div>
+                            <div class="price">
+                                20 990
+                                <i class="fa fa-rub" aria-hidden="true"></i>
+                            </div>
+                        </a>
+                        <a href="#" class="tab_item">
+                            <div class="descr">
+                                <h4>Стандарт</h4>
+                                <p>
+                                    This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh
+                                    vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
+                                    nisi elit consequat
+                                </p>
+                            </div>
+                            <div class="price">
+                                25 990
+                                <i class="fa fa-rub" aria-hidden="true"></i>
+                            </div>
+                        </a>
+                        <a href="#" class="tab_item">
+                            <div class="descr">
+                                <h4>Премиум</h4>
+                                <p>
+                                    This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh
+                                    vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
+                                    nisi elit consequat
+                                </p>
+                            </div>
+                            <div class="price">
+                                30 990
+                                <i class="fa fa-rub" aria-hidden="true"></i>
+                            </div>
+                        </a>
+                        <a href="#" class="tab_item">
+                            <div class="descr">
+                                <h4>VIP</h4>
+                                <p>
+                                    This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh
+                                    vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
+                                    nisi elit consequat
+                                </p>
+                            </div>
+                            <div class="price">
+                                50 990
+                                <i class="fa fa-rub" aria-hidden="true"></i>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="contract_content">
+                <a href="#" class = "contract_item">
+                    <div class="image_wrapper">
+                        <img src="/img/icons/calculator.png" alt="icon">
+                    </div>
+                    <div class="text_wrapper">
+                        <h4>
+                            РАСЧЕТ стоимости
+                            РИТУАЛЬНЫх УСЛУГ
+                        </h4>
+                        <p>
+                            This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh
+                            vel velit auctor aliquet. Aenean
+                        </p>
+                    </div>
+                </a>
+                <h2>
+                    Наши агенты незамедлительно
+                    займутся всем необходимым.
+                    Они готовы:
+
+                </h2>
+                <ul class="features">
+                    <li><span>Организовать отправку тела морг;</span></li>
+                    <li><span>Подготовить все необходимые документы;</span></li>
+                    <li><span>Обеспечить бальзамирование, если на это будет воля родственников;</span></li>
+                    <li><span>Организовать погребение или кремацию;</span></li>
+                    <li><span>Обеспечить транспортом процессию;</span></li>
+                    <li><span>Организовать поминальный обед.</span></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<!--SECTION MAIN SERVICES PAGE PRICE END-->
+
+<!--SECTION SERVICE PAGE TEXT CONTENT START-->
+<section class="service_page_text_wrapper">
+    <div class="container">
+        <h2>Пользуйтесь услугами опытных профессионалов</h2>
+        <p>
+            При организации достойных похорон наши агенты готовы предложить различные тарифы.
+            <b>Выбор любого из них гарантирует отсутствие необходимости беспокоиться о решении
+                каких-либо вопросов.</b> С нашей помощью вы сможете приобрести гроб и любую другую
+            ритуальную продукцию. Похороны – это горький и печальный процесс для родственников
+            умершего, поэтому мы стараемся избавить их от любых хлопот. Мы никогда не навязываем
+            услуги и предлагаем клиентам самостоятельно выбрать только те, которые они сочтут
+            необходимыми.
+        </p>
+        <p>
+            <span class="underline">Наши агенты готовы незамедлительно выехать к вам для консультации, которая
+            предоставляется на бесплатной основе.</span> Агентство «Горслужба-Ритуал» располагает
+            всем необходимым, чтобы обеспечить достойные проводы усопшего в последний путь.
+            Для сопровождения похорон мы готовы представить любой необходимый автотранспорт.
+            Родственники и близкие умершего будут доставлены и к месту захоронения (кремации),
+            и к месту поминального обеда.
+        </p>
+    </div>
+</section>
+<!--SECTION SERVICE PAGE TEXT CONTENT END-->
+
 <!--SECTION SERVICE PAGE CONTRACT START-->
 <section class="service_page_contract_wrapper">
     <div class="container">
