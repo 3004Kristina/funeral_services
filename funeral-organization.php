eee52e9c9c22b754f912e0d27df2fd4f78e9c143
3004Kristina <kristina.usova@gmail.com>
2020-12-17 16:09:51 +0300

    add service page

--- funeral-organization.php
+++ funeral-organization.php
@@ -1,5 +1,271 @@
 <?php require __DIR__ . '/.header.php'; ?>
 
+<!--SECTION FUNERAL ORGANIZATION PAGE INFO CONTENT START-->
+<section class="funeral_organization_page_info_wrapper">
+    <div class="container">
+        <ul class="breadcrumbs">
+            <li><a href="/">Главная</a></li>
+            <li><a href="/">Ритуальные услуги</a></li>
+            <li><a href="#" class="active">Организация похорон</a></li>
+        </ul>
+        <h2>Ритуальные услуги в САО</h2>
+        <div class="funeral_organization_page_info">
+            <div class="info_item">
+                <p>
+                    Городская служба «Ритуал» оказывает помощь в подготовке и проведении
+                    траурных церемоний. Вежливые сотрудники готовы выехать по адресу или
+                    предоставить консультацию 24 часа в сутки. Ритуальные услуги от компании
+                    «Ритуал»
+                </p>
+
+                <p>
+                    Мы готовы предложить помощь в:
+                </p>
+                <ul class="info_list">
+                    <li><span>Оформлении документов (медицинских, гербовых, для кладбища, крематория);</span></li>
+                    <li><span>Организации похорон (поиск места на кладбище, предоставление транспорта, услуги грузчиков и оркестра);</span></li>
+                    <li><span>Подготовке тела (к погребению, бальзамированию, кремации);</span></li>
+                    <li><span>В перевозке ГРУЗА 200 и т.д.</span></li>
+                </ul>
+                <div class="btn_wrapper">
+                    <button class="btn">Вызов ритуального агента ОНЛАЙН</button>
+                    <a href="/" class="btn grey">Вызов ритуального агента ОНЛАЙН</a>
+                </div>
+            </div>
+            <div class="info_item">
+                <img src="/img/organiz.png" alt="image">
+            </div>
+        </div>
+    </div>
+</section>
+<!--SECTION FUNERAL ORGANIZATION PAGE INFO CONTENT END-->
+
+<!--SECTION FUNERAL ORGANIZATION PAGE MORGUES LIST START-->
+<section class="funeral_organization_page_morgues_list_wrapper">
+    <div class="container">
+        <h2>Близлежащие объекты</h2>
+        <table>
+            <tr class="main">
+                <td>Морг</td>
+                <td>Телефон и адрес</td>
+                <td>Часы работы</td>
+            </tr>
+            <tr>
+                <td>Морг больницы №50</td>
+                <td>г. Москва, ул. Вучетича д. 21</td>
+                <td>будни  9:00 до 15:00 суббота — с 9:00
+                    до 13:00, воскресение — выходной
+                </td>
+            </tr>
+            <tr>
+                <td>Морг больницы им. С.П. Боткина</td>
+                <td>г. Москва, 2-й Боткинский проезд, дом 5</td>
+                <td>
+                    будни  9:00 до 15:00 суббота — с 9:00
+                    до 13:00, воскресение — выходной
+                </td>
+            </tr>
+            <tr>
+                <td>Морг Судебно-медицинский №10</td>
+                <td>г. Москва, 2-й Боткинский проезд, дом 5, корпус 33</td>
+                <td>
+                    будни  9:00 до 15:00 суббота — с 9:00
+                    до 13:00, воскресение — выходной
+                </td>
+            </tr>
+            <tr>
+                <td>Морг больницы №81</td>
+                <td>г. Москва, ул. Лобненская, 10</td>
+                <td>
+                    будни  9:00 до 15:00 суббота — с 9:00
+                    до 13:00, воскресение — выходной
+                </td>
+            </tr>
+        </table>
+    </div>
+</section>
+<!--SECTION FUNERAL ORGANIZATION PAGE MORGUES LIST END-->
+
+<!--SECTION FUNERAL ORGANIZATION PAGE MAP START-->
+<section class="funeral_organization_page_map_wrapper">
+    <div class="container">
+        <h2>Горслужба Ритуал оказывает услуги по адресу</h2>
+    </div>
+    <div class="map">
+        <div id="myMapId" style="width:100%; height:100%"></div>
+    </div>
+</section>
+<!--SECTION FUNERAL ORGANIZATION PAGE MAP END-->
+
+<!--SECTION FUNERAL ORGANIZATION PAGE LOCATION START-->
+<section class="funeral_organization_page_location_wrapper">
+    <div class="container">
+        <div class="funeral_organization_page_location">
+            <ul class="prices_navigation_tabs">
+                <li><a href="#tab1" class="active"><span>ГОРОДА ПОДМОСКОВЬЯ</span></a></li>
+                <li><a href="#tab2"><span>Москва</span></a></li>
+            </ul>
+
+            <div class="prices_content_tabs">
+                <div class="tab" id="tab1">
+                    <ul class="features">
+                        <li><span>Апрелевка</span></li>
+                        <li><span>Балашиха</span></li>
+                        <li><span>Бронницы</span></li>
+                        <li><span>Верея</span></li>
+                        <li><span>Видное</span></li>
+                        <li><span>Волоколамск</span></li>
+                        <li><span>Воскресенск</span></li>
+                        <li><span>Высоководск</span></li>
+                        <li><span>Голицыно</span></li>
+                        <li><span>Дедовск</span></li>
+                        <li><span>Дмитров</span></li>
+                        <li><span>Долгопрудный</span></li>
+                        <li><span>Домодедово</span></li>
+                        <li><span>Домодедово</span></li>
+                        <li><span>Дрезна</span></li>
+                        <li><span>Дубна</span></li>
+                        <li><span>Егорьевск</span></li>
+                        <li><span>Железнодорожный</span></li>
+                        <li><span>Жуковский</span></li>
+                    </ul>
+                    <ul class="features">
+                        <li><span>Зарайск</span></li>
+                        <li><span>Звенигород</span></li>
+                        <li><span>Зеленоград</span></li>
+                        <li><span>Ивантеевка</span></li>
+                        <li><span>Истра</span></li>
+                        <li><span>Кашира</span></li>
+                        <li><span>Климовск</span></li>
+                        <li><span>Клин</span></li>
+                        <li><span>Коломна</span></li>
+                        <li><span>Конаково</span></li>
+                        <li><span>Королёв</span></li>
+                        <li><span>Красногорск</span></li>
+                        <li><span>Краснозаводск</span></li>
+                        <li><span>Краснознаменск</span></li>
+                        <li><span>Кубинка</span></li>
+                        <li><span>Куровское</span></li>
+                        <li><span>Ликино-Дулево</span></li>
+                        <li><span>Лобня</span></li>
+                        <li><span>Лосино-Петровский</span></li>
+                    </ul>
+                    <ul class="features">
+                        <li><span>Луховицы</span></li>
+                        <li><span>Лыткарино</span></li>
+                        <li><span>Люберцы</span></li>
+                        <li><span>Марьино</span></li>
+                        <li><span>Митино</span></li>
+                        <li><span>Можайск</span></li>
+                        <li><span>Московский</span></li>
+                        <li><span>Мытищи</span></li>
+                        <li><span>Наро-Фоминск</span></li>
+                        <li><span>Нахабино</span></li>
+                        <li><span>Ногинск</span></li>
+                        <li><span>Одинцово</span></li>
+                        <li><span>Ожерелье</span></li>
+                        <li><span>Озеры</span></li>
+                        <li><span>Орехово-Зуево</span></li>
+                        <li><span>Павловский-Посад</span></li>
+                        <li><span>Пересвет</span></li>
+                        <li><span>Петровский</span></li>
+                        <li><span>Пущино</span></li>
+                    </ul>
+                    <ul class="features">
+                        <li><span>Раменское</span></li>
+                        <li><span>Реутов</span></li>
+                        <li><span>Рошаль</span></li>
+                        <li><span>Руза</span></li>
+                        <li><span>Сергиев-Посад</span></li>
+                        <li><span>Серпухов</span></li>
+                        <li><span>Солнечногорск</span></li>
+                        <li><span>Старая Купавна</span></li>
+                        <li><span>Ступино</span></li>
+                        <li><span>Талдом</span></li>
+                        <li><span>Троицк</span></li>
+                        <li><span>Фрязино</span></li>
+                        <li><span>Химки</span></li>
+                        <li><span>Хотьково</span></li>
+                        <li><span>Черноголовка</span></li>
+                        <li><span>Чехов</span></li>
+                        <li><span>Шатура</span></li>
+                        <li><span>Щелково</span></li>
+                    </ul>
+                </div>
+
+                <div class="tab" id="tab2" style="display: none">
+                    <ul class="features">
+                        <li><span>Академический</span></li>
+                        <li><span>Алексеевский</span></li>
+                        <li><span>Алтуфьевский</span></li>
+                        <li><span>Арбат</span></li>
+                        <li><span>Аэропорт</span></li>
+                        <li><span>Бабушкинский</span></li>
+                        <li><span>Басманный</span></li>
+                        <li><span>Беговой</span></li>
+                        <li><span>Бескудниковский</span></li>
+                        <li><span>Бибирево</span></li>
+                        <li><span>Бирюлёво Восточное</span></li>
+                        <li><span>Бирюлёво Западное</span></li>
+                        <li><span>Богородское</span></li>
+                        <li><span>Братеево</span></li>
+                    </ul>
+                    <ul class="features">
+                        <li><span>Академический</span></li>
+                        <li><span>Алексеевский</span></li>
+                        <li><span>Алтуфьевский</span></li>
+                        <li><span>Арбат</span></li>
+                        <li><span>Аэропорт</span></li>
+                        <li><span>Бабушкинский</span></li>
+                        <li><span>Басманный</span></li>
+                        <li><span>Беговой</span></li>
+                        <li><span>Бескудниковский</span></li>
+                        <li><span>Бибирево</span></li>
+                        <li><span>Бирюлёво Восточное</span></li>
+                        <li><span>Бирюлёво Западное</span></li>
+                        <li><span>Богородское</span></li>
+                        <li><span>Братеево</span></li>
+                    </ul>
+                    <ul class="features">
+                        <li><span>Академический</span></li>
+                        <li><span>Алексеевский</span></li>
+                        <li><span>Алтуфьевский</span></li>
+                        <li><span>Арбат</span></li>
+                        <li><span>Аэропорт</span></li>
+                        <li><span>Бабушкинский</span></li>
+                        <li><span>Басманный</span></li>
+                        <li><span>Беговой</span></li>
+                        <li><span>Бескудниковский</span></li>
+                        <li><span>Бибирево</span></li>
+                        <li><span>Бирюлёво Восточное</span></li>
+                        <li><span>Бирюлёво Западное</span></li>
+                        <li><span>Богородское</span></li>
+                        <li><span>Братеево</span></li>
+                    </ul>
+                    <ul class="features">
+                        <li><span>Академический</span></li>
+                        <li><span>Алексеевский</span></li>
+                        <li><span>Алтуфьевский</span></li>
+                        <li><span>Арбат</span></li>
+                        <li><span>Аэропорт</span></li>
+                        <li><span>Бабушкинский</span></li>
+                        <li><span>Басманный</span></li>
+                        <li><span>Беговой</span></li>
+                        <li><span>Бескудниковский</span></li>
+                        <li><span>Бибирево</span></li>
+                        <li><span>Бирюлёво Восточное</span></li>
+                        <li><span>Бирюлёво Западное</span></li>
+                        <li><span>Богородское</span></li>
+                        <li><span>Братеево</span></li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--SECTION FUNERAL ORGANIZATION PAGE LOCATION END-->
+
 <!--SECTION FUNERAL ORGANIZATION PAGE TEXT CONTENT START-->
 <section class="funeral_organization_page_text_wrapper">
     <div class="container">
---
+++ service.php
@@ -0,0 +1,100 @@
+<?php require __DIR__ . '/.header.php'; ?>
+
+<!--SECTION FUNERAL ORGANIZATION PAGE INFO CONTENT START-->
+<section class="funeral_organization_page_info_wrapper">
+    <div class="container">
+        <ul class="breadcrumbs">
+            <li><a href="/">Главная</a></li>
+            <li><a href="/">Ритуальные услуги</a></li>
+            <li><a href="#" class="active">Организация похорон</a></li>
+        </ul>
+        <h2>Организация похорон</h2>
+        <div class="funeral_organization_page_info">
+            <div class="info_item">
+                <img src="/img/serv.png" alt="image">
+            </div>
+            <div class="info_item">
+                <p>
+                    Известие о смерти близкого человека всегда застает врасплох.
+                    Беда нарушает привычное течение дел и душевное равновесие родственников усопшего.
+                    Возникает множество проблем и вопросов, связанных с оформлением и организацией похорон.
+                    Справиться с ними, находясь в состоянии отчаяния, нелегко.
+                    <b>Специалисты нашей ритуальной службы помогут в тяжёлую минуту и выполнят свою работу безупречно.</b>
+                    Организация похорон пройдет четко, без спешки и суеты.
+                </p>
+
+                <p>
+                    Профессиональная организация похорон – это возможность для родственников умершего переложить
+                    все организационные вопросы на плечи представителя нашей службы.
+                    <b>Специалисты агентства «Горслужба-Ритуал» готовы взять на себя абсолютно все хлопоты.</b> В городской
+                    специализированной службе Ritual.ru данная услуга предоставляется бесплатно и круглосуточно.
+                </p>
+
+                <div class="btn_wrapper">
+                    <button class="btn">Вызов ритуального агента ОНЛАЙН</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--SECTION FUNERAL ORGANIZATION PAGE INFO CONTENT END-->
+
+<!--SECTION SERVICE PAGE CONTRACT START-->
+<section class="service_page_contract_wrapper">
+    <div class="container">
+        <div class="service_page_contract">
+            <div class="contract_text">
+                <h4>Посмотрите отзывы о нашей работе</h4>
+                <p>
+                    Краткое описание предложения
+                </p>
+            </div>
+            <div class="contract_action">
+                <button class="btn">
+                    Посмотреть отзывы
+                </button>
+            </div>
+        </div>
+    </div>
+</section>
+<!--SECTION SERVICE PAGE CONTRACT END-->
+
+<!--SECTION MAIN SERVICES PAGE ADVANTAGES START-->
+<section class="main_services_page_advantages_wrapper">
+    <div class="container">
+        <h2>Наши преимущества</h2>
+        <div class="main_services_page_advantages">
+            <div class="advantages_item">
+                <img src="/img/icons/advan1.png" alt="icon">
+                <h2>ОПЫТ</h2>
+                <p>
+                    Более 15 лет занимаемся<br> ритуальными услугами
+                </p>
+            </div>
+            <div class="advantages_item">
+                <img src="/img/icons/advan2.png" alt="icon">
+                <h2>Профессионализм</h2>
+                <p>
+                    Более 15 лет занимаемся<br> ритуальными услугами
+                </p>
+            </div>
+            <div class="advantages_item">
+                <img src="/img/icons/advan3.png" alt="icon">
+                <h2>Надежность</h2>
+                <p>
+                    Более 15 лет занимаемся<br> ритуальными услугами
+                </p>
+            </div>
+            <div class="advantages_item">
+                <img src="/img/icons/advan4.png" alt="icon">
+                <h2>Лицензия росстандарт</h2>
+                <p>
+                    Более 15 лет занимаемся<br> ритуальными услугами
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+<!--SECTION MAIN SERVICES PAGE ADVANTAGES END-->
+<?php require __DIR__ . '/.reference-info.php'; ?>
+<?php require __DIR__ . '/.footer.php'; ?>
