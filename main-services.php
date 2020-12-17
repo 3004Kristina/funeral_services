6d5bf786c6946781623294aa780d07c0cc0098ff
3004Kristina <kristina.usova@gmail.com>
2020-12-17 11:31:56 +0300

    add funeral-organization page

--- main-services.php
+++ main-services.php
@@ -3,36 +3,37 @@
 <section class="main_services_page_banner_wrapper">
     <div class="container">
         <div class="main_services_page_banner">
-            <h1>Организация похорон в Москве
+            <h1>Организация похорон в Москве<br>
                 и Московской области</h1>
             <p>
                 Lorem ipsum dolor sit amet, consectetur deradipiscing elit. Nulla quam velit, vulputate eu pharetra nec.
                 mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id
             </p>
-            <div class="main_services_page_banner_list">
-                <a href="#">
-                    <img src="/img/icons/cemetery.png" alt="icon">
-                    <div class="services_text">
-                        <h4>Экстренная ситуация</h4>
-                        <p>Человек умер. Что делать?</p>
-                    </div>
-                </a>
-                <a href="#">
-                    <img src="/img/icons/diploma.png" alt="icon">
-                    <div class="services_text">
-                        <h4>Требуемые документы</h4>
-                        <p>Для организации похорон</p>
-                    </div>
-                </a>
-                <a href="#">
-                    <img src="/img/icons/skidka.png" alt="icon">
-                    <div class="services_text">
-                        <h4>Льготы и скидки</h4>
-                        <p>Положенные государством</p>
-                    </div>
-                </a>
+        </div>
+        <div class="main_services_page_banner_list">
+            <div class="item">
+                <img src="/img/icons/cemetery.png" alt="icon">
+                <div class="services_text">
+                    <h4>Экстренная ситуация</h4>
+                    <p>Человек умер. Что делать?</p>
+                </div>
+            </div>
+            <div class="item">
+                <img src="/img/icons/diploma.png" alt="icon">
+                <div class="services_text">
+                    <h4>Требуемые документы</h4>
+                    <p>Для организации похорон</p>
+                </div>
+            </div>
+            <div class="item">
+                <img src="/img/icons/skidka.png" alt="icon">
+                <div class="services_text">
+                    <h4>Льготы и скидки</h4>
+                    <p>Положенные государством</p>
+                </div>
             </div>
         </div>
+
     </div>
 </section>
 <!--SECTION MAIN SERVICES PAGE BANNER END-->
@@ -57,7 +58,7 @@
             <div class="license">
                 <img src="/img/license.png" alt="image">
                 <p>
-                    Городская служба «РИТУАЛ»  осуществляет свою деятельность на основании
+                    Городская служба «РИТУАЛ» осуществляет свою деятельность на основании
                     Федерального закона от 12.01.1996 г. №8-ФЗ «О погребении
                     и похоронном деле» ( с изменениями и дополнениями ), Закона г.
                     Москвы от 04.06.1997 г. №11 «О погребении и похоронном деле в
@@ -74,6 +75,354 @@
     </div>
 </section>
 <!--SECTION MAIN PAGE SEO CONTENT END-->
+
+<!--SECTION MAIN PAGE CONTRACT START-->
+<section class="main_services_page_contract_wrapper">
+    <div class="container">
+        <div class="main_page_contract">
+            <div class="contract_text">
+                <h4>Что делать если умер близкий человек?</h4>
+                <p>
+                    Порядок действий в экстренных ситуациях
+                </p>
+            </div>
+
+        </div>
+    </div>
+</section>
+<!--SECTION MAIN PAGE CONTRACT END-->
+
+<!--SECTION MAIN SERVICES PAGE LIST START-->
+<section class="main_services_page_list_wrapper">
+    <div class="container">
+        <h2>Ритуальные услуги в Москве и Московской области</h2>
+        <div class="main_services_page_list">
+            <div class="services_item">
+                <a href="#">
+                    <img src="/img/ritualnyiagent-1.jpg" alt="image">
+                    <p>Ритуальный агент</p>
+                </a>
+            </div>
+            <div class="services_item">
+                <a href="#">
+                    <img src="/img/pic_103-840x510-crop.jpg" alt="image">
+                    <p>Организация похорон</p>
+                </a>
+            </div>
+            <div class="services_item">
+                <a href="#">
+                    <img src="/img/70526933.jpg" alt="image">
+                    <p>Кремация</p>
+                </a>
+            </div>
+            <div class="services_item">
+                <a href="#">
+                    <img src="/img/perevozka-umershih-v-ukrainu.jpg" alt="image">
+                    <p>Груз 200</p>
+                </a>
+            </div>
+            <div class="services_item">
+                <a href="#">
+                    <img src="/img/1626118_ORIGINAL_1518003689.jpg" alt="image">
+                    <p>Место на кладбище</p>
+                </a>
+            </div>
+            <div class="services_item">
+                <a href="#">
+                    <img src="/img/legalization-of-documents_2.jpg" alt="image">
+                    <p>Оформление документов</p>
+                </a>
+            </div>
+            <div class="services_item">
+                <a href="#">
+                    <img src="/img/Orthodox_funeral_service.jpg" alt="image">
+                    <p>Религиозное обслуживание</p>
+                </a>
+            </div>
+            <div class="services_item">
+                <a href="#">
+                    <img src="/img/img_3399.jpg" alt="image">
+                    <p>Ритуальный транспорт</p>
+                </a>
+            </div>
+            <div class="services_item">
+                <a href="#">
+                    <img src="/img/35311_604_420_1.jpg" alt="image">
+                    <p>Ритуальные товары</p>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+<!--SECTION MAIN SERVICES PAGE LIST END-->
+
+<!--SECTION MAIN SERVICES PAGE DECOR START-->
+<section class="main_services_page_decor_wrapper">
+    <div class="container">
+        <div class="main_page_contract">
+            <div class="contract_text">
+                <h4>Интернет магазин ритуальных товаров</h4>
+                <p>
+                    Скидки и акции на все категории товаров
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+<!--SECTION MAIN SERVICES PAGE DECOR END-->
+
+<!--SECTION MAIN SERVICES PAGE PRICE START-->
+<section class="main_services_page_price_wrapper">
+    <div class="container">
+        <div class="prices_contract_wrapper">
+        <div class="prices_content">
+            <ul class="prices_navigation_tabs">
+                <li><a href="#tab1" class="active"><span>Стоимость захоронения</span></a></li>
+                <li><a href="#tab2"><span>Стоимость кремации</span></a></li>
+            </ul>
+
+            <div class="prices_content_tabs">
+                <div class="tab" id="tab1">
+                    <a href="#" class="tab_item">
+                        <div class="descr">
+                            <h4>Базовый</h4>
+                            <p>
+                                This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh
+                                vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
+                                nisi elit consequat
+                            </p>
+                        </div>
+                        <div class="price">
+                            13 990
+                            <i class="fa fa-rub" aria-hidden="true"></i>
+                        </div>
+                    </a>
+                    <a href="#" class="tab_item">
+                        <div class="descr">
+                            <h4>Стандарт</h4>
+                            <p>
+                                This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh
+                                vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
+                                nisi elit consequat
+                            </p>
+                        </div>
+                        <div class="price">
+                            13 990
+                            <i class="fa fa-rub" aria-hidden="true"></i>
+                        </div>
+                    </a>
+                    <a href="#" class="tab_item">
+                        <div class="descr">
+                            <h4>Премиум</h4>
+                            <p>
+                                This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh
+                                vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
+                                nisi elit consequat
+                            </p>
+                        </div>
+                        <div class="price">
+                            13 990
+                            <i class="fa fa-rub" aria-hidden="true"></i>
+                        </div>
+                    </a>
+                    <a href="#" class="tab_item">
+                        <div class="descr">
+                            <h4>VIP</h4>
+                            <p>
+                                This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh
+                                vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
+                                nisi elit consequat
+                            </p>
+                        </div>
+                        <div class="price">
+                            13 990
+                            <i class="fa fa-rub" aria-hidden="true"></i>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="tab" id="tab2" style="display: none">
+                    <a href="#" class="tab_item">
+                        <div class="descr">
+                            <h4>Базовый</h4>
+                            <p>
+                                This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh
+                                vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
+                                nisi elit consequat
+                            </p>
+                        </div>
+                        <div class="price">
+                            20 990
+                            <i class="fa fa-rub" aria-hidden="true"></i>
+                        </div>
+                    </a>
+                    <a href="#" class="tab_item">
+                        <div class="descr">
+                            <h4>Стандарт</h4>
+                            <p>
+                                This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh
+                                vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
+                                nisi elit consequat
+                            </p>
+                        </div>
+                        <div class="price">
+                            25 990
+                            <i class="fa fa-rub" aria-hidden="true"></i>
+                        </div>
+                    </a>
+                    <a href="#" class="tab_item">
+                        <div class="descr">
+                            <h4>Премиум</h4>
+                            <p>
+                                This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh
+                                vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
+                                nisi elit consequat
+                            </p>
+                        </div>
+                        <div class="price">
+                            30 990
+                            <i class="fa fa-rub" aria-hidden="true"></i>
+                        </div>
+                    </a>
+                    <a href="#" class="tab_item">
+                        <div class="descr">
+                            <h4>VIP</h4>
+                            <p>
+                                This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh
+                                vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
+                                nisi elit consequat
+                            </p>
+                        </div>
+                        <div class="price">
+                            50 990
+                            <i class="fa fa-rub" aria-hidden="true"></i>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+            <div class="contract_content">
+                <a href="#" class = "contract_item">
+                    <div class="image_wrapper">
+                        <img src="/img/icons/calculator.png" alt="icon">
+                    </div>
+                    <div class="text_wrapper">
+                        <h4>
+                            РАСЧЕТ стоимости
+                            РИТУАЛЬНЫх УСЛУГ
+                        </h4>
+                        <p>
+                            This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh
+                            vel velit auctor aliquet. Aenean
+                        </p>
+                    </div>
+                </a>
+                <a href="#" class = "contract_item">
+                    <div class="image_wrapper">
+                        <img src="/img/icons/contract.png" alt="icon">
+                    </div>
+                    <div class="text_wrapper">
+                        <h4>
+                            Прижизненый
+                            договор
+                        </h4>
+                        <p>
+                            This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh
+                            vel velit auctor aliquet. Aenean lorem quis bibendum auctor,
+                            nisi elit consequat
+                        </p>
+                    </div>
+                </a>
+                <a href="#" class = "contract_item">
+                    <div class="image_wrapper">
+                        <img src="/img/icons/contract.png" alt="icon">
+                    </div>
+                    <div class="text_wrapper">
+                        <h4>
+                            ГБУ Ритуал
+                            договор №
+                        </h4>
+                        <p>
+                            This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh
+                            vel velit auctor aliquet. Aenean lorem quis bibendum auctor,
+                            nisi elit consequat lorem quis bibendum auctor,
+
+                        </p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+<!--SECTION MAIN SERVICES PAGE PRICE END-->
+
+<!--SECTION MAIN SERVICES PAGE PARTNERS START-->
+<section class="main_services_page_partners_wrapper">
+    <div class="container">
+        <div class="main_services_page_partners">
+            <a href="#">
+                <img src="/img/partners1.png" alt="partners">
+            </a>
+            <a href="#">
+                <img src="/img/partners2.png" alt="partners">
+            </a>
+            <a href="#">
+                <img src="/img/partners3.png" alt="partners">
+            </a>
+            <a href="#">
+                <img src="/img/partners4.png" alt="partners">
+            </a>
+            <a href="#">
+                <img src="/img/partners5.png" alt="partners">
+            </a>
+            <a href="#">
+                <img src="/img/partners6.png" alt="partners">
+            </a>
+            <a href="#">
+                <img src="/img/partners7.png" alt="partners">
+            </a>
+        </div>
+    </div>
+</section>
+<!--SECTION MAIN SERVICES PAGE PARTNERS END-->
+
+<!--SECTION MAIN SERVICES PAGE ADVANTAGES START-->
+<section class="main_services_page_advantages_wrapper">
+    <div class="container">
+        <h2>Наши преимущества</h2>
+        <div class="main_services_page_advantages">
+            <div class="advantages_item">
+                <img src="/img/icons/advan1.png" alt="icon">
+                <h2>ОПЫТ</h2>
+                <p>
+                    Более 15 лет занимаемся<br> ритуальными услугами
+                </p>
+            </div>
+            <div class="advantages_item">
+                <img src="/img/icons/advan2.png" alt="icon">
+                <h2>Профессионализм</h2>
+                <p>
+                    Более 15 лет занимаемся<br> ритуальными услугами
+                </p>
+            </div>
+            <div class="advantages_item">
+                <img src="/img/icons/advan3.png" alt="icon">
+                <h2>Надежность</h2>
+                <p>
+                    Более 15 лет занимаемся<br> ритуальными услугами
+                </p>
+            </div>
+            <div class="advantages_item">
+                <img src="/img/icons/advan4.png" alt="icon">
+                <h2>Лицензия росстандарт</h2>
+                <p>
+                    Более 15 лет занимаемся<br> ритуальными услугами
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+<!--SECTION MAIN SERVICES PAGE ADVANTAGES END-->
 
 <?php require __DIR__ . '/.reference-info.php'; ?>
 
